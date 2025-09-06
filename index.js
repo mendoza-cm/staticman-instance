b22d34d182b972e2aa0e489f360b80b6f4822d35
Christina Mendoza <mendozachristina1729@gmail.com>
2025-09-06 15:50:08 -0600

    Fix: blind staticman to Heroku port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const port = process.env.PORT || 3000;
+
 try {
   const StaticmanAPI = require('./server')
   const api = new StaticmanAPI()
 
-  api.start(port => {
+  api.start(port, () => {
     console.log('Staticman API running on port', port)
   })
 } catch (e) {
   console.error(e)
-}
